62916f6bfc40fe380edeb88494fcf4f43eeb5cd9
Nick-Sercel <nickcsercel@gmail.com>
2024-10-25 17:02:33 -0700

    yes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { CountryComponent } from './country/country.component';
 import { CityComponent } from './city/city.component';
 import { WeatherComponent } from './weather/weather.component';
+import { CountryPopulationComponent } from './country/country-population.component';
 
 export const routes: Routes = [
     {
         path: 'countries',
         component: CountryComponent
+    },
+    {
+        path: "countriesPopulation/:id",
+        component: CountryPopulationComponent
     },
     {
         path: 'cities',
--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment/environment.developments';
+
+export interface Country {
+  id: number;
+  name: string;
+  iso2: string;
+  iso3: string;
+}
 
 @Component({
   selector: 'app-country',
@@ -7,6 +16,19 @@ import { Component } from '@angular/core';
   templateUrl: './country.component.html',
   styleUrl: './country.component.scss'
 })
-export class CountryComponent {
+export class CountryComponent implements OnInit {
+  public countries: Country[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getCountries();
+  }
+  getCountries() {
+    this.http.get<Country[]>(`${environment.baseUrl}api/Countries`).subscribe(
+      {
+        next: result => this.countries = result,
+        error: e => console.error(e)
+      }
+    )
+  }
 
 }
